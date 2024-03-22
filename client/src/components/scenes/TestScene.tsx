16ab6812c8648ed41b93ebeb901b0b37f6b467ab
Abraham Furlan <abrahamfurlan@gmail.com>
2024-03-22 00:42:47 -0400

    Implemented TestScene

--- client/src/components/scenes/TestScene.tsx
+++ client/src/components/scenes/TestScene.tsx
@@ -1,19 +1,119 @@
 import { Grid, CameraControls, AccumulativeShadows, RandomizedLight, Stars, Sky } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
-import { memo, useRef } from "react";
+import { memo, useEffect, useRef } from "react";
 
 const TestScene = (props) => {
-    const meshRef = useRef()
-    const cameraControlsRef = useRef<CameraControls | null>(null);
+    const cameraControlsRef = useRef<CameraControls>(null);
+    let wPreviousTime;
+    let wInterval;
+    let aPreviousTime;
+    let aInterval;
+    let sPreviousTime;
+    let sInterval;
+    let dPreviousTime;
+    let dInterval;
 
-    const { camera } = useThree()
+    useEffect(() => {
+        if (cameraControlsRef.current == null) {
+            return
+        }
+
+        console.log("Camera Controls: ", cameraControlsRef.current);
+
+        document.addEventListener("keydown", (e) => {
+            const newTime = Date.now();
+            console.log(e.key);
+
+            if (e.key === 'w') {
+                if (wInterval == null) {
+                    wPreviousTime = newTime;
+                    wInterval = setInterval(() => {
+                        console.log("wKeyHeld");
+                        const newTime = Date.now();
+                        const wDeltaTime = newTime - wPreviousTime;
+                        wPreviousTime = newTime;
+                        cameraControlsRef.current!.forward(0.01 * wDeltaTime, true);
+                    }, 10);
+                }
+            }
+
+            if (e.key === "a") {
+                if (aInterval == null) {
+                    aPreviousTime = newTime;
+                    aInterval = setInterval(() => {
+                        console.log("aKeyHeld");
+                        const newTime = Date.now();
+                        const aDeltaTime = newTime - aPreviousTime;
+                        aPreviousTime = newTime;
+                        cameraControlsRef.current!.truck(-0.01 * aDeltaTime, 0, true);
+                    })
+                }
+            }
+
+            if (e.key === "s") {
+                if (sInterval == null) {
+                    sPreviousTime = newTime;
+                    sInterval = setInterval(() => {
+                        console.log("sKeyHeld");
+                        const newTime = Date.now();
+                        const sDeltaTime = newTime - sPreviousTime;
+                        sPreviousTime = newTime;
+                        cameraControlsRef.current!.forward(-0.01 * sDeltaTime, true);
+                    })
+                }
+            }
+
+            if (e.key === "d") {
+                if (dInterval == null) {
+                    dPreviousTime = newTime;
+                    dInterval = setInterval(() => {
+                        console.log("dKeyHeld");
+                        const newTime = Date.now();
+                        const dDeltaTime = newTime - dPreviousTime;
+                        dPreviousTime = newTime;
+                        cameraControlsRef.current!.truck(0.01 * dDeltaTime, 0, true);
+                    })
+                }
+            }
+        });
+
+
+        document.addEventListener("keyup", (e) => {
+            switch (e.key) {
+                case "w":
+                    console.log("wKeyUp");
+                    clearInterval(wInterval);
+                    wInterval = null;
+                    break;
+                case "a":
+                    console.log("aKeyUp");
+                    clearInterval(aInterval);
+                    aInterval = null;
+                    break;
+                case "s":
+                    console.log("sKeyUp");
+                    clearInterval(sInterval);
+                    sInterval = null;
+                    break;
+                case "d":
+                    console.log("dKeyUp");
+                    clearInterval(dInterval);
+                    dInterval = null;
+                    break;
+                default:
+                    return;
+            }
+        });
+
+        cameraControlsRef.current.mouseButtons.left = 0;
+        cameraControlsRef.current.mouseButtons.right = 1;
+        cameraControlsRef.current.mouseButtons.middle = 1;
+    }, [cameraControlsRef]);
 
     return (
         <group position-y={-0.5}>
             <Ground />
             {/* <Shadows /> */}
             <Atmosphere />
-            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
             <Controls cameraControlsRef={cameraControlsRef} />
         </group>
     );
@@ -30,9 +130,6 @@ const Controls = (props) => {
 
     const control = <CameraControls ref={props.cameraControlsRef} {...controlConfig} />;
 
-    props.cameraControlsRef.current.mouseButtons.left = 2;
-    props.cameraControlsRef.current.mouseButtons.right = 1;
-
     return control;
 }
 
@@ -46,7 +143,13 @@ const Atmosphere = () => {
         rayleigh: 0,
         turbidity: 1,
     }
-    return <Sky sunPosition={[1, .5, 0]} {...skyConfig} />
+
+    return (
+        <>
+            <Stars radius={75} depth={100} count={10000} factor={7} saturation={5} fade speed={1} />
+            <Sky sunPosition={[1, .5, 0]} {...skyConfig} />
+        </>
+    )
 }
 
 const Ground = () => {
@@ -60,7 +163,7 @@ const Ground = () => {
         fadeDistance: 30,
         fadeStrength: 1,
         followCamera: false,
-        infiniteGrid: true
+        infiniteGrid: true,
     }
     return <Grid position={[0, -0.01, 0]} args={[10.5, 10.5]} {...gridConfig} />
 }
